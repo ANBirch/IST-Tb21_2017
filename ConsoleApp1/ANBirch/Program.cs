1e5b2f50524d0a95098191be2ef44e4a9546b887
ANBirch <anbirch@mail.ru>
2017-03-28 20:22:39 +0300

    Добавлен проект ANBirch

---
+++ ConsoleApp1/ANBirch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANBirch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Привет мир!!!);
+            Console.ReadKey(true);
+
+        }
+    }
+}
